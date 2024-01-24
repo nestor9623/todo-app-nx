4ebedb259b75da63af751e0be1f1e832a1201466
EX366505 <nestorandres.franco@empresas.justicia.es>
2024-01-24 11:43:59 +0100

    Added jest config
    config global for jest and fix bug, change the access token for Nx

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import 'zone.js';
+import 'zone.js/testing';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
